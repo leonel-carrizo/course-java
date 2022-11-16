74cf1d32d6fb1572142480a9b191dfec2f204ff9
Leonel Carrizo <102891741+leonel-carrizo@users.noreply.github.com>
2022-11-16 04:50:40 +0000

    Segunda clase

---
+++ hello-op/src/com/company/ByWolrd.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class ByWolrd {
+    public static void main(String[] args) {
+        System.out.println("Adios desde clase ByWorld");
+    }
+}
---
+++ hello-op/src/com/company/HelloWrold.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class HelloWrold {
+
+    public static void main(String[] args){
+    System.out.println("Hola mundo desde la clase HelloWorld");
+    }
+}
